ed11f3e98667cf02710f442a59a64a25b3144abe
vandana bhoyar <vandanabhoyar1995@gmail.com>
2026-07-28 18:24:50 +0530

    this is my second commit

--- tests/day1.spec.ts
+++ tests/day1.spec.ts
@@ -73,7 +73,7 @@ await page.locator('.oxd-select-text-input').click();
 await page.locator('.oxd-select-dropdown').getByText('Software Engineer').click();
 await page.getByPlaceholder('Type here').nth(0).fill('van@gmail.com');
 await page.getByPlaceholder('Type here').nth(1).fill('8830930741');
-//await page.locator(".oxd-file-button").setInputFiles('C:\\PlaywrightDemo\\tests\\resume\\Vandana_Bhoyar_SDET4.4_Resume.pdf');
+await page.locator(".oxd-file-button").setInputFiles('C:\\PlaywrightDemo\\tests\\resume\\Vandana_Bhoyar_SDET4.4_Resume.pdf');
 //const filePath = "C:\\PlaywrightDemo\\tests\\resume\\Vandana_Bhoyar_SDET4.4_Resume.pdf";
 //await page.locator("input.oxd-file-input").setInputFiles(filePath);
 //console.log("Exists:", fs.existsSync(filePath));
